b9e8438a0ddab04f8d39e252b99c495875dde535
Farai Chakarisa <fmc@bt-industrial.co.za>
2024-05-03 15:31:03 +0200

    Add Relations

--- app/Models/Organisation.php
+++ app/Models/Organisation.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Organisation extends Model
 {
     use HasFactory;
+
+    public function department(): HasMany
+    {
+        return $this->hasMany('department');
+    }
+
+    public function user(): HasMany
+    {
+        return $this->hasMany('user');
+    }
+
 }
--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Timesheet extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('user');
+    }
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo('status');
+    }
 }
