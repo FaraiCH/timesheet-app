8811989e00e200e0d7aa7f1e3c94cf8bf5068069
Farai Chakarisa <fmc@bt-industrial.co.za>
2024-10-16 22:09:30 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+require dirname(__DIR__) . '/app-modules/module-routes-manifest.php';
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -22,8 +22,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('timesheet', \App\Http\Controllers\TimesheetController::class);
 });
 
 require __DIR__.'/auth.php';
