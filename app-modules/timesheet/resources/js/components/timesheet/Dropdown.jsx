09d23657175219399fff3988f5561e169eb664c7
Farai Chakarisa <fmc@bt-industrial.co.za>
2024-10-30 20:04:18 +0200

    Update Dropdown Component to include mapped array data

--- app-modules/timesheet/resources/js/components/timesheet/Dropdown.jsx
+++ app-modules/timesheet/resources/js/components/timesheet/Dropdown.jsx
@@ -3,6 +3,8 @@ function Dropdown(props)
 {
     return <select className="form-select" aria-label="Default select example">
         <option value="none">Choose</option>
+
+        // Map data to list options on the list
         {props.mapData.map((content) => (
             <option value={content}>{content}</option>
         ))}
