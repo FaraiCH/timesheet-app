690a04a401e16a9f4fda92db43e0d9a3f4c09c67
Farai Chakarisa <fmc@bt-industrial.co.za>
2024-10-31 13:10:54 +0200

    Add Comments

--- app-modules/timesheet/resources/js/components/timesheet/dropdownContent/content.js
+++ app-modules/timesheet/resources/js/components/timesheet/dropdownContent/content.js
@@ -1,6 +1,10 @@
+// Here data that will we be giving our dropdowns
+// This will keep our data de-coupled from the component
+// This will also make it easier to replace with api endpoint responses in future
+
 const shift = ['Day', 'Night'];
 
-const comment = [
+const comment = {
     'Annual Leave',
     'Sick Leave',
     'Family Responsibility',
@@ -8,6 +12,6 @@ const comment = [
     'Off Without Pay',
     'Public Holiday Not Worked',
     'Public Holiday Worked'
-]
+}
 
 export { shift, comment };
