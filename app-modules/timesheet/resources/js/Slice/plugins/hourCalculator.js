13954d2b64fd765efce7744b328319daf1502b50
Farai Chakarisa <fmc@bt-industrial.co.za>
2024-10-31 19:28:27 +0200

    Fix DoubleTime

--- app-modules/timesheet/resources/js/Slice/plugins/hourCalculator.js
+++ app-modules/timesheet/resources/js/Slice/plugins/hourCalculator.js
@@ -10,7 +10,7 @@ const calculateHours = (date, timeStart, timeEnd, hourRange) => {
     let hours = Math.abs(d1 - d2) / 36e5;
 
     //Check to see if it's Sunday or Saturday with 9 hours
-    if(dayName === 'Sunday' || (dayName === 'Saturday' && hourRange === 9))
+    if(dayName === 'Sunday' || (dayName === 'Saturday' && hourRange == 9))
     {
         // There is no overtime if these criteria are met
         return 0
