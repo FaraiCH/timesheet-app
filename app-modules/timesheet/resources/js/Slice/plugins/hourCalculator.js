117f5b168c2a335e26679662b63d6ee72003e25b
Farai Chakarisa <fmc@bt-industrial.co.za>
2024-10-31 11:11:20 +0200

    Add Comments

--- app-modules/timesheet/resources/js/Slice/plugins/hourCalculator.js
+++ app-modules/timesheet/resources/js/Slice/plugins/hourCalculator.js
@@ -1,12 +1,16 @@
 const calculateHours = (date, timeStart, timeEnd, hourRange) => {
+    // Convert the date from datepicker and times from timepicker to a date object
     const d1 = new Date(date + " " + convertTime(timeStart));
     const d2 = new Date(date + " " + convertTime(timeEnd));
+    // Calculate the different in hours
     let hours = Math.abs(d1 - d2) / 36e5;
     if(hours > hourRange){
+        // We check if time is over the hour range to calculate the remaining difference
         return hours - hourRange;
     }
     else
     {
+        // Return this if overtime is not reached
         return 0;
     }
 }
--- app-modules/timesheet/resources/js/Slice/plugins/startOfWeek.js
+++ app-modules/timesheet/resources/js/Slice/plugins/startOfWeek.js
@@ -1,8 +1,9 @@
 const getStartOfWeek = (date) => {
+    // Use this to get the Monday of every week with the date format
     const givenDate = new Date(date);
     const day = givenDate.getDay(); // Sunday is 0, Monday is 1, and so on
     const diff = givenDate.getDate() - day + (day === 0 ? -6 : 1); // Adjust when day is Sunday
-    return new Date(givenDate.setDate(diff));
+    return new Date(givenDate.setDate(diff)); // We return a date object of the Monday in the current week
 }
 
 export default getStartOfWeek
