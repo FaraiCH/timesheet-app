e5e45350daa1e9b9b58feb2c3b8164a134d73973
Farai Chakarisa <fmc@bt-industrial.co.za>
2025-03-31 12:14:19 +0200

    Update App name to Clock In

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,7 +3,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Clock In';
 
 // Here we referencing not only the current resoruces directory, but also our app-modules where we are developing our functions
 
